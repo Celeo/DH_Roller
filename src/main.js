75f7b958c2cb939f6ca3240d801e16bc48c28e19
Celeo <celeodor@gmail.com>
2017-03-19 19:30:44 -0700

    Added Firebase stubs

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
-import App from './App'
 import router from './router'
-
+import App from './App'
+import Firebase from 'firebase'
+import VueFire from 'vuefire'
 
 import 'materialize-css/dist/css/materialize.css'
 import 'materialize-css/dist/js/materialize.js'
 
+
+Vue.use(VueFire)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  render: h => h(App),
+  firebase: {
+    // TODO
+  }
 })
